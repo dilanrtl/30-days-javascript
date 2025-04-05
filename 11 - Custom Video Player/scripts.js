5caa1442053ba79146b74b8b38e26295ba095d7e
Dilan <dilaleynartl@gmail.com>
2025-04-05 15:37:46 +0300

    new commit for 11. practice

--- 11 - Custom Video Player/scripts.js
+++ 11 - Custom Video Player/scripts.js
@@ -57,3 +57,5 @@ progress.addEventListener('click', scrub);
 progress.addEventListener('mousemove', (e) => mousedown && scrub(e));
 progress.addEventListener('mousedown', () => mousedown = true);
 progress.addEventListener('mouseup', () => mousedown = false);
+
+.
